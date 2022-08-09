a98ac4b2ec7ace75b1c4b96463d9ed569ca00c27
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-08-09 00:29:36 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+let producto = document.getElementById("producto");
+let precio = document.getElementById("precio");
+let agregar = document.getElementById("btn-agregar");
+let calcular = document.getElementById("btn-calcular");
+let lista = document.getElementById("lista");
+let total = document.getElementById("total");
+let vaciar = document.getElementById("vaciar");
+
+/*arreglo string para cargar los productos, (vacío) sin cantidades*/
+
+let productos: string[] = [];
+
+/*arreglo number para cargar los precios, (vacío) sin cantidades*/
+
+let precios: number[] = [];
+/* arreglo para sumar el total*/
+let suma: number = 0;
+
+/*FUNCIÓN "const"- metodo push para agregar un elemento al final del arreglo (asignar evento), 
+es decir: cuando haga clik al arreglo producto (línea 10), le vamos a pushear el valor ingresado al imput "Producto" */
+const agregarAlCarrito = () => {
+  //let miproducto : string = producto.value
+  productos.push(producto.value);
+  precios.push(Number(precio.value));
+
+  lista?.innerHTML += `<li>${producto.value} : $${precio.value}</li>`;
+  //vaciar los campos de entrada
+  producto.value = "";
+  precio.value = "";
+};
+
+const calcularTotal = () => {
+  suma = 0;
+  for (let i: number = 0; i < precios.length; i++) {
+    suma += Number(precios[i]);
+  }
+  total?.innerHTML = suma;
+};
+
+const vaciarLista = () => {
+  lista?.innerHTML = "";
+  total.innerHTML = "";
+};
+//agregar eventos a los botones
+agregar?.addEventListener("click", agregarAlCarrito);
+calcular?.addEventListener("click", calcularTotal);
+vaciar?.addEventListener("click", vaciarLista);
